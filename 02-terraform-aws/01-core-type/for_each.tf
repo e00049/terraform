9e5652f0b522e3cd0e45862557fc96c7549d477a
e00049 <105837578+e00049@users.noreply.github.com>
2023-02-28 21:26:38 +0530

    Update for_each.tf

--- 02-terraform-aws/01-core-type/for_each.tf
+++ 02-terraform-aws/01-core-type/for_each.tf
@@ -27,3 +27,21 @@ locals {
     sg_8080   = aws_security_group.sg_8080.id,
   }
 }
+
+ output "instance_id" {
+   description = "ID of the EC2 instance"
+-   value       = aws_instance.web_app.id
++   value       = [for instance in aws_instance.web_app: instance.id]
+ }
+
+ output "instance_public_ip" {
+   description = "Public IP address of the EC2 instance"
+-   value       = aws_instance.web_app.public_ip
++   value       = [for instance in aws_instance.web_app: instance.public_ip]
+ }
+
+output "instance_name" {
+   description = "Tags of the EC2 instance"
+-  value        = aws_instance.web_app.tags
++  value        = [for instance in aws_instance.web_app: instance.tags.Name]
+}
